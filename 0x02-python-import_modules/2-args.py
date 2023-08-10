f6ed83317f101bb047c3dfdc9367fcaf301d7287
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-10 17:17:26 +0000

    feat: code should not be executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 import sys
+from calculator_1 import add, sub, mul, div
 
-argc = len(sys.argv) - 1
-argv = sys.argv[1:]
+a = 10
+b = 5
 
-if argc == 0:
-    print("Number of argument(s): 0.")
-else:
-    plural = "s" if argc > 1 else ""
-    print(f"Number of argument(s): {argc}, argument{plural}:",
-          end="\n" if argc == 1 else "s:\n")
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
 
-    for i, arg in enumerate(argv, start=1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, result_add))
+    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+    print("{:d} / {:d} = {:d}".format(a, b, result_div))
+
+    argc = len(sys.argv) - 1
+    argv = sys.argv[1:]
+
+    if argc == 0:
+        print("Number of argument(s): 0.")
+    else:
+        plural = "s" if argc > 1 else ""
+        print(f"Number of argument(s): {argc}, argument{plural}:",
+              end="\n" if argc == 1 else "s:\n")
+
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
