aa490187d6c19a3b300a96e7006043b595f59a3d
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-10 10:03:43 +0000

    feat: a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+print(f"{a:d} + {a:d} = {add(a, b)}")
+print(f"{a:d} + {a:d} = {sub(a, b)}")
+print(f"{a:d} + {a:d} = {mul(a, b)}")
+print(f"{a:d} + {a:d} = {div(a, b)}")
