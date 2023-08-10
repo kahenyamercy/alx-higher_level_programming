fc882b6e5619baccc66c730d632112706a9530f6
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-10 17:31:08 +0000

    feat: code should not be executed when imported

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-
-import sys
 from calculator_1 import add, sub, mul, div
+import sys
 
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
 
-def usage_and_exit():
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-if len(sys.argv) != 4:
-    usage_and_exit()
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-
-if operator == "+":
-    result = add(a, b)
-elif operator == "-":
-    result = sub(a, b)
-elif operator == "*":
-    result = mul(a, b)
-elif operator == "/":
-    result = div(a, b)
-else:
-    print("Unknown operator")
-    usage_and_exit()
-
-print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, result))
