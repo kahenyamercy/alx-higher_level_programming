b90d1b68765b1f15617410f201a105ce6eb32f35
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-10 17:23:25 +0000

    feat: code should not be executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
-
-a = 10
-b = 5
-
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
 
 if __name__ == "__main__":
-    operations = [
-        ("+", result_add),
-        ("-", result_sub),
-        ("*", result_mul),
-        ("/", result_div)
-    ]
-
-    for operator, result in operations:
-        print(f"{a} {operator} {b} = {result}")
-
     argc = len(sys.argv) - 1
     argv = sys.argv[1:]
 
     if argc == 0:
-        print("Number of argument(s): 0.")
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
     else:
-        plural = "s" if argc > 1 else ""
-        print(f"Number of argument(s): {argc}, argument{plural}:",
-              end="\n" if argc == 1 else "s:\n")
-
+        print("{} arguments:".format(argc))
         for i, arg in enumerate(argv, start=1):
-            print(f"{i}: {arg}")
+            print("{:d}: {}".format(i, arg))
