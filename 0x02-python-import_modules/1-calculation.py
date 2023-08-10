88a0b86f035329b0037c567b3538fe49e6e537bb
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-10 17:04:11 +0000

    feat: code should not be executed when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
+from calculator_1 import add, sub, mul, div
 
-    print(f"{a:d} + {a:d} = {add(a, b)}")
-    print(f"{a:d} + {a:d} = {sub(a, b)}")
-    print(f"{a:d} + {a:d} = {mul(a, b)}")
-    print(f"{a:d} + {a:d} = {div(a, b)}")
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, result_add))
+    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+    print("{:d} / {:d} = {:d}".format(a, b, result_div))
