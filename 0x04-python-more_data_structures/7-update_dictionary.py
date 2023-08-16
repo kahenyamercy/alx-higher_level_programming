d2e95a05f9ab69fde2f337f8f7fbc1be7f0b9f43
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-16 16:13:32 +0000

    feat:  a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
