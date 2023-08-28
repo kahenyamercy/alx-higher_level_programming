79ef064a1be255fb16f23668d8d665b0289dc9f0
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-28 17:17:18 +0000

    remove finally

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             element_printed += 1
     except IndexError:
         pass
-    finally:
-        print()
+
+    print()
     return element_printed
