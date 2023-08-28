df17d037441bf063c9974e04b2830e9bca31fd9c
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-28 17:35:46 +0000

    updates

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     elements_printed = 0
-    try:
-        for i range(x):
+    for i range(x):
+        try:
             print(my_list[i], end='')
             element_printed += 1
-    except IndexError:
-        pass
-
+        except IndexError:
+            break
     print()
     return element_printed
