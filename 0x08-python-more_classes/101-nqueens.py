28f4f1219b6a46110d25aca57b8ca72ab3ef793e
kahenyamercy <kahenyamercy5@gmail.com>
2023-09-06 17:07:39 +0000

    n-queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -48,3 +48,16 @@ if __name__ == "__main__":
             a[i][1] = None
 
     def nqueens(x):
+
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
+
+    # start the recursive process at x = 0
+    nqueens(0)
