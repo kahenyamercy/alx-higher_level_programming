f79813bbaf3663061c125c970611e5ed5114d33f
kahenyamercy <kahenyamercy5@gmail.com>
2023-09-12 22:27:54 +0000

    update script

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -33,3 +33,24 @@ if __name__ == "__main__":
                 count = 1
             else:
                 count += 1
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
