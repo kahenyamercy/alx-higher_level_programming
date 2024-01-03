1056f46ade19cde4939712c11134f0f71827cdc0
kahenyamercy <kahenyamercy5@gmail.com>
2024-01-03 20:36:21 +0000

    script that displays the status code of a get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-
-/* Import the 'request' module for making HTTP requests*/
 const request = require('request');
-
-/* Extract the URL from the command-line argument */
 const url = process.argv[2];
-
-/* Make an HTTP request to the specified URL*/
 request(url, function (err, response) {
   if (err) {
-    /* If an error occurs during the request, print the error*/
-    console.log('Error:', err);
+    console.log(err);
   } else {
-    /* If the request is successful, print the HTTP status code*/
-    console.log('HTTP Status Code:', response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
